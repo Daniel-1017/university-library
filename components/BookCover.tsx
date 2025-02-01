238e7f466b5bb79f66e803530c28649db8d86472
Daniel-1017 <frimudaniel17@gmail.com>
2025-02-01 16:59:03 +0100

    fixed cover bug and added BookList and BookCard components

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -9,8 +9,8 @@ const variantStyles: Record<BookCoverVariant, string> = {
     extraSmall: "book-cover_extra_small",
     small: "book-cover_small",
     medium: "book-cover_medium",
-    regular: "book-cover_extra_regular",
-    wide: "book-cover_extra_wide",
+    regular: "book-cover_regular",
+    wide: "book-cover_wide",
 };
 
 interface Props {
@@ -23,7 +23,7 @@ interface Props {
 const BookCover = ({ className, variant = "regular", coverColor = "#012B48", coverUrl = "https://placehold.co/400x600.png" }: Props) => {
     return (
         <div className={cn("relative transition-all duration-300", variantStyles[variant], className)}>
-            <BookCoverSvg coverColor='coverColor' />
+            <BookCoverSvg coverColor={coverColor} />
             <div className='absolute z-10' style={{ left: "12%", width: "87.5%", height: "88%" }}>
                 <Image src={coverUrl} alt='Book cover' fill className='rounded-sm object-fill' />
             </div>
