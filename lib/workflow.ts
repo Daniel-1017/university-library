a180fcf5d8294b728102eda0fac8579ff0c2b453
Daniel-1017 <frimudaniel17@gmail.com>
2025-02-02 22:13:22 +0100

    added sendEmail function to workflow

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,25 @@
 import config from "@/lib/config";
 import { Client as WorkflowCLient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkflowCLient({
     baseUrl: config.env.upstash.qstashUrl,
     token: config.env.upstash.qstashToken,
 });
+
+const qstashClient = new QStashClient({ token: config.env.upstash.qstashUrl });
+
+export const sendEmail = async ({ email, subject, message }: { email: string; subject: string; message: string }) => {
+    await qstashClient.publishJSON({
+        api: {
+            name: "email",
+            provider: resend({ token: config.env.resendToken }),
+        },
+        body: {
+            from: "Daniel Frimu <hello.danielfrimu.com>",
+            to: [email],
+            subject,
+            html: message,
+        },
+    });
+};
